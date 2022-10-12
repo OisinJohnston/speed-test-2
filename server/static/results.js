c842942751f913e69041d43c7f5b68ea6117b73c
gamingdiamond982 <oj.nologic@gmail.com>
2022-10-12 23:47:25 +0100

    bug fix

--- server/static/results.js
+++ server/static/results.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 	let restable = document.getElementById("singleresults");
-	fetch(url = "/api/singleentries", {
+	fetch(url = "./api/singleentries", {
 		method: 'GET'
 	}).then(async function(response) {
             var resp = await response.json();
@@ -22,7 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
         	}
     	})
 	let twotable = document.getElementById("tworesults");
-	fetch(url = "/api/twoentries", {method:"GET"}).then(async function(response) {
+	fetch(url = "./api/twoentries", {method:"GET"}).then(async function(response) {
 		var resp = await response.json();
 		console.log(resp)
 		for (var i = 0; i<resp.length; i++) {
